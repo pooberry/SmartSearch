db50723b5417091399c909f6eec1db29be75ced2
mhowe21 <48158483+mhowe21@users.noreply.github.com>
2019-03-20 10:10:03 -0600

    Update options.js

--- options.js
+++ options.js
@@ -1,10 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () 
-{
-  document.getElementById("tokenEnterButton").addEventListener("click", getStore);
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("tokenEnterButton").addEventListener("click", tStore);
 });
-function getStore()
+
+function tStore()
 {
-   var tkn= tokenStore.value;
-   console.log(tkn);
+   var token2 = document.getElementById("tokenInputBox").value;
+     
+   chrome.storage.local.set({"token": token2}, function()
+   {
+     console.log("value is " + token2);
+   });
 
 }
