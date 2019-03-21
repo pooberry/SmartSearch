32c14c44ec746b8a7bcbc42f78ac4f5ecd2d6ca1
Mason Howe <mhowe@corp.instructure.com>
2019-03-21 15:29:06 -0600

    TokenRemove1

--- options.js
+++ options.js
@@ -9,6 +9,21 @@ function tStore()
    chrome.storage.local.set({"token": token2}, function()
    {
      console.log("value is " + token2);
+     alert("Token Stored succesfully")
    });
 
 }
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("ClearToken").addEventListener("click", tRemove);
+
+});
+
+function tRemove()
+{
+  chrome.storage.local.remove(["token"],function()
+
+  {
+    console.log("Token Removed");
+    alert("token cleared successfully");
+  })
+};
