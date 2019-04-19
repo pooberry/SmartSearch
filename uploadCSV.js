2cb0cb126de9496ff0d53d0ef142875de0b98c4e
Mason Howe <mhowe@corp.instructure.com>
2019-04-18 19:07:20 -0600

    ParseCSV

--- uploadCSV.js
+++ uploadCSV.js
@@ -1,11 +1,52 @@
+var CSVFile;
+//var parsedResults;
+
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("submitFile").addEventListener("click", FileStore);
-  });
+    document.getElementById("submitFile").addEventListener("click", Run2);
+});
 
-  function FileStore()
-  {
-      const CSVFile = document.getElementById("file").files[0];
-      console.log(CSVFile.name);
-      window.close();
 
-  }
+
+function Run2() {
+    FileStore();
+    FileParse().then((message) =>{
+        for(var i = 0; i < message.length; i++)
+        {
+            console.log("name" + message[i].name);
+            console.log("domain " + message[i].domain);
+            console.log("auth " + message[i].auth);
+        }
+    }).catch((message) => {
+        console.log(message);
+    })
+}
+
+function FileStore() {
+    CSVFile = document.getElementById("file").files[0];
+    console.log(CSVFile.name);
+
+}
+
+function FileParse() {
+    return new Promise(function (resolve, reject){
+        Papa.parse(CSVFile, {
+            header: true,
+            dynamicTyping: true,
+            complete: function (results) {
+                //parsedResults = results;
+                console.log(results);
+                if(results.length != 0 || results.length !=undefined){
+                    resolve(results);
+                }
+                else{
+                    reject(error("CSV was not able to be parsed correctly"));
+                }
+
+                
+    
+            }
+    
+        });
+    })
+    
+}
