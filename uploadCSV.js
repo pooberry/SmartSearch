0978d25637729962c6c11a2e3965080773509191
Mason Howe <mhowe@corp.instructure.com>
2019-04-22 14:51:03 -0600

    CSVRun

--- uploadCSV.js
+++ uploadCSV.js
@@ -2,20 +2,21 @@ var CSVFile;
 var CSVName;
 var CSVDomain;
 var CSVAuth;
+//var token;
 //var parsedResults;
+
+getChromeVariables();
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("submitFile").addEventListener("click", Run2);
-});
-
-
+});//event listiner for form submission. 
 
 function Run2() {
     FileStore();
-    FileParse().then((message) =>{
+    FileParse().then((message) => {
         let data = message;
         console.log(data.data);
-        for(var i = 0; i < data.data.length; i++)//
+        for (var i = 0; i < data.data.length; i++) //
         {
             CSVName = data.data[i].name;
             CSVDomain = data.data[i].domain;
@@ -26,9 +27,20 @@ function Run2() {
             console.log(CSVDomain);
             console.log(CSVAuth);
 
+            SubmitRequest2(CSVName, CSVDomain, CSVAuth).then((message)=>{
+                console.log(message);
+
+            }).catch((message)=>{
+
+                console.log(message);
+            })
+
+
+
+
         }
-        
-       
+
+
     }).catch((message) => {
         let data = message
         alert(data);
@@ -42,25 +54,84 @@ function FileStore() {
 }
 
 function FileParse() {
-    return new Promise(function (resolve, reject){
+    return new Promise(function (resolve, reject) {
         Papa.parse(CSVFile, {
             header: true,
             dynamicTyping: true,
             complete: function (results) {
                 //parsedResults = results;
                 //console.log(results);
-                if(results.length != 0 || results.length !=undefined){
+                if (results.length != 0 || results.length != undefined) {
                     resolve(results);
-                }
-                else{
+                } else {
                     reject(error("CSV was not able to be parsed correctly"));
                 }
 
-                
-    
+
+
             }
-    
+
         });
     })
+
+}
+
+function getChromeVariables()
+{
     
+chrome.storage.local.get(["token"], function (result) {
+    token = result.token;
+    })
+}
+
+function SubmitRequest2(name, domain, auth) {
+    return new Promise(function (resolve,reject) {
+        if (auth != null || auth != undefined || auth != "") {
+            var data = new FormData();
+            data.append("account_domain_lookup[name]", name);
+            data.append("account_domain_lookup[domain]", domain);
+            data.append("account_domain_lookup[authentication_provider]", auth);
+
+            var xhr = new XMLHttpRequest();
+            xhr.withCredentials = true;
+
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === 4) {
+                    console.log(this.responseText);
+                    let responseCode = this.status;
+                    resolve("Run status " + responseCode);
+                }
+            });
+
+            xhr.open("POST", "https://siteadmin.instructure.com/api/v1/account_domain_lookups/");
+            xhr.setRequestHeader("Authorization", "Bearer " + token);
+            
+            xhr.send(data);
+
+        }
+        if(auth == null || auth == undefined || auth == "")
+        {
+            var data = new FormData();
+            data.append("account_domain_lookup[name]", name);
+            data.append("account_domain_lookup[domain]", domain);
+            
+            var xhr = new XMLHttpRequest();
+            xhr.withCredentials = true;
+
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === 4) {
+                    console.log(this.responseText);
+                    let responseCode = this.status;
+                    
+                    resolve("Run status " + responseCode);
+                }
+            });
+
+            xhr.open("POST", "https://siteadmin.instructure.com/api/v1/account_domain_lookups/");
+            xhr.setRequestHeader("Authorization", "Bearer " + token);
+            
+            xhr.send(data);
+        }
+
+    })
 }
