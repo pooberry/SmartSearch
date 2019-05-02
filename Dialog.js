4766f7e4545f0e8204e63eb7e2d3616b30fe4799
Mason Howe <mhowe@corp.instructure.com>
2019-05-02 14:18:00 -0600

    DBT1

--- Dialog.js
+++ Dialog.js
@@ -1,3 +1,5 @@
+var Action;
+
 console.log("Dialog JS loaded");
 document.addEventListener("DOMContentLoaded", function(){
   document.getElementById("process").addEventListener("click", getProcessButton);
